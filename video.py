816f32475e51532d8c70f6d5b759e1b2e6e1a495
Evan Arsenault <evan.arsenault85@gmail.com>
2019-11-25 10:20:47 -0500

    improved video code

--- video.py
+++ video.py
@@ -7,19 +7,33 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 ## LOAD IMAGE
 matplotlib.use('TKAgg')
+
+def unsharp_mask(image,k1,k2):
+    gaussian_image = cv2.GaussianBlur(image,(3,3),0)
+    return cv2.addWeighted(image, k1, gaussian_image, k2, 0)
+
 bottom_pixel_frames = []
 
-cap = cv2.VideoCapture('./WIN_20191115_14_17_02_Pro.mp4')
+cap = cv2.VideoCapture('./coop60fps.mov')
 count = 0
 while(cap.isOpened()):
+
     ret,image = cap.read()
-            
+    
+    if count == 0:
+        roi = cv2.selectROI(image)
+
+    image = image[int(roi[1]):int(roi[1]+roi[3]),int(roi[0]):int(roi[0]+roi[2])]
+
     r = 600.0/image.shape[1]
     dim = (600, int(image.shape[0]*r))
 
     # image = cv2.resize(image, dim, interpolation = cv2.INTER_AREA)
     
     image_gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+    
+    #image sharpening
+    image_gray = unsharp_mask(image_gray,1.5,-1)
 
     image_can = cv2.Canny(image_gray,100,200)
     #print(image_can)
@@ -39,7 +53,7 @@ while(cap.isOpened()):
         print(pixel_c)
 
     cv2.imshow('frame', image_can)
-# count = count + 1
+    count = count + 1
 # cv2.imshow('frame',image_can)
     if cv2.waitKey(25) & 0xFF == ord('q'):
         break
